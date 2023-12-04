62db17e77f9ea7ff3e345e0bf2498025fc26f54c
Thanh-Quy Nguyen <thannguy@bouyguestelecom.fr>
2023-12-04 15:30:52 +0100

    :white_check_mark: Add tests to get/create/edit tweets

---
+++ test/Twitter.test.js
@@ -0,0 +1,46 @@
+const { expect } = require('chai');
+const { describe, it, beforeEach } = require('mocha');
+const { ethers } = require('hardhat');
+
+describe.only('Twitter', () => {
+  let twitter;
+  let owner;
+  let otherAccount;
+
+  beforeEach(async () => {
+    [owner, otherAccount] = await ethers.getSigners();
+
+    const Twitter = await ethers.getContractFactory('Twitter');
+    twitter = await Twitter.deploy();
+  });
+
+  it('should get an empty initial state', async () => {
+    const tweets = await twitter.getTweets();
+    expect(tweets.length).to.equal(0);
+  });
+
+  it('should create a tweet', async () => {
+    await twitter.createTweet('Hello, world!');
+    const tweets = await twitter.getTweets();
+
+    expect(tweets.length).to.equal(1);
+    expect(tweets[0].content).to.equal('Hello, world!');
+  });
+
+  it("should be able to edit one's own tweet", async () => {
+    await twitter.createTweet('Hello, world!');
+    await twitter.editTweet(0, 'Goodbye, world!');
+    const tweets = await twitter.getTweets();
+
+    expect(tweets.length).to.equal(1);
+    expect(tweets[0].content).to.equal('Goodbye, world!');
+  });
+
+  it("should not be able to edit someone else's tweet", async () => {
+    await twitter.connect(owner).createTweet('Hello, world!');
+
+    await expect(
+      twitter.connect(otherAccount).editTweet(0, 'Goodbye, world!'),
+    ).to.be.revertedWith('You are not the author of this tweet');
+  });
+});
